4633e21e2bb351e80aa31caeefe19d42c633a7da
Oraichain <oraichain@ORAICHAINS-MACBOOK-PRO.local>
2025-03-07 11:03:03 +0700

    update fix bot mot luc

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TIME-MANAGEMENT-AGENT
+
+
+## Lên agent
+Tải requiremenst.txt về trước, sau đó lên agent:
+
+```
+docker-compose up --build
+```
+
+check `localhost:5001/docs` xem hdsd 
--- src/routers/chat.py
+++ src/routers/chat.py
@@ -6,13 +6,19 @@ from core.chat import generate_chat_completions
 
 router = APIRouter(prefix="/chat", tags=["chat"])
 
+class ChatHistoryItem(BaseModel):
+    user: str
+    assistant: str
+
 class Query(BaseModel):
     message: str
-    history: List[Dict[str,str]] 
+    history: List[ChatHistoryItem]
+
 
 @router.post("/chat_completion")
 async def chat_completions(query: Query):
+    history_list = [item.dict() for item in query.history]
     # return StreamingResponse(generate_chat_completions(query.message))
     return {
-        "message": await generate_chat_completions(query.message, query.history)
+        "message": await generate_chat_completions(query.message, history_list)
     }
